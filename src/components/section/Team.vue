50566d5398a955a835c0a406e8d4b4e1517b02a6
mtaufikbaskoro <mtaufikbaskoro@gmail.com>
2025-10-27 00:16:45 +0700

    add vue3-carousel to the project

--- src/components/section/Team.vue
+++ src/components/section/Team.vue
@@ -2,6 +2,8 @@
 import SectionLayout from '@/layouts/SectionLayout.vue'
 import { ref } from 'vue'
 import Card from '../shared/Card.vue'
+import Carousel from '@/components/shared/Carousel.vue'
+import { Slide } from 'vue3-carousel'
 
 import ridwanImg from '@/assets/img/ridwan.jpeg'
 import agungImg from '@/assets/img/agung.jpeg'
@@ -21,10 +23,13 @@ const data = ref([
 <template>
   <SectionLayout class-names="flex-col">
     <h1 class="w-full text-4xl font-bold capitalize text-center">Team Kami</h1>
-    <div class="w-full max-w-5xl grid grid-cols-5 gap-12">
-      <div v-for="(item, index) in data" :key="index" class="col-span-1">
+    <Carousel>
+      <Slide v-for="(item, index) in data" :key="index">
         <Card :img="item.img" :title="item.title" :description="item.description" />
-      </div>
-    </div>
+      </Slide>
+    </Carousel>
+    <!-- <div v-for="(item, index) in data" :key="index" class="col-span-1">
+        <Card :img="item.img" :title="item.title" :description="item.description" />
+      </div> -->
   </SectionLayout>
 </template>
---
+++ src/components/shared/Carousel.vue
@@ -0,0 +1,53 @@
+<script setup>
+import 'vue3-carousel/carousel.css'
+import { Carousel, Pagination, Navigation } from 'vue3-carousel'
+
+const config = {
+  height: 360,
+  itemsToShow: 1,
+  gap: 5,
+  snapAlign: 'center',
+
+  // 'breakpointMode' determines how the carousel breakpoints are calculated
+  // Acceptable values: 'viewport' (default) | 'carousel'
+  // 'viewport' - breakpoints are based on the viewport width
+  // 'carousel' - breakpoints are based on the carousel width
+  breakpointMode: 'carousel',
+
+  // Breakpoints are mobile-first
+  // Any settings not specified will fall back to the carousel's default settings
+  breakpoints: {
+    // 300px and up
+    300: {
+      itemsToShow: 2,
+      snapAlign: 'center',
+    },
+    // 400px and up
+    400: {
+      itemsToShow: 3,
+      snapAlign: 'start',
+    },
+    // 500px and up
+    1000: {
+      itemsToShow: 5,
+      snapAlign: 'start',
+    },
+  },
+}
+</script>
+
+<template>
+  <Carousel class="w-full" v-bind="config">
+    <slot />
+    <template #addons>
+      <Navigation />
+    </template>
+  </Carousel>
+</template>
+
+<style>
+.carousel {
+  --vc-nav-background: rgba(255, 255, 255, 0.7);
+  --vc-nav-border-radius: 100%;
+}
+</style>
